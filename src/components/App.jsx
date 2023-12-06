888965d564c8fd5c502094913da6be4c94b350ca
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-12-06 13:57:39 +0100

    added mobile first places and photos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import leoch from '../images/leoch.png';
 //import ls from '../services/localStorage';
 //import callToApi from '../services/api';
 import '../styles/App.scss';
-import { format } from 'date-fns';
+import {format} from 'date-fns';
 //import { useState, useEffect } from "react";
 
 function App() {
@@ -31,25 +31,30 @@ function App() {
         <section className="section-principal">
           <div className="section-principal__img"></div>
           <div className="section-principal__couple">
-            <p className="section-principal__couple--name">
-              Jamie
-            </p>
+            <p className="section-principal__couple--name">Jamie</p>
             <p className="section-principal__couple--and">&</p>
             <p className="section-principal__couple--name">Claire</p>
             <p className="section-principal__date">07.06.1743</p>
           </div>
-          
+
           {/* <h1 className="section-principal__couple">
             Jamie & Claire
           </h1>
           <h2 className="section-principal__date">07.06.1743</h2> */}
         </section>
-        <section>
-          <a href="https://maps.app.goo.gl/jUXnbtMkMD7exVN48" target="_blank">
-            <img src={church} alt="" />
-            <p>Ceremonia</p>
-            <p>Glencorse Old Kirk</p>
-            <p>17:00</p>
+        <section className="section-places">
+          <div className="ceremony">
+            <p className="ceremony__title">Ceremonia</p>
+            <a href="https://maps.app.goo.gl/jUXnbtMkMD7exVN48" target="_blank">
+              <img
+                src={church}
+                alt="Iglesia de Glencorse"
+                className="ceremony__img"
+              />
+            </a>
+            
+            <p className="ceremony__church">Glencorse Old Kirk</p>
+            <p className="ceremony__time">17:00</p>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d142898.81512788942!2d-3.4851392145780817!3d55.970272242171795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4887c0f6ea190cf1%3A0x264c80a0965af674!2sGlencorse%20House!5e0!3m2!1ses!2ses!4v1698691184832!5m2!1ses!2ses"
               width="400"
@@ -57,44 +62,48 @@ function App() {
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
+              className="ceremony__map"
             ></iframe>
-          </a>
-          <a href="https://maps.app.goo.gl/UNmd2EcyWEU4KLT68" target="_blank">
-            <img src={leoch} alt="" className='celebration__img'/>
-            <p>Celebración</p>
-            <p>Castillo de Doune</p>
-            <p>20:00</p>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2220.3748460488637!2d-4.0525207230910185!3d56.185198873247344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48888bd24de4fcbf%3A0x5599d30a61b7fb39!2sCastillo%20de%20Doune!5e0!3m2!1ses!2ses!4v1698691886282!5m2!1ses!2ses"
-              width="400"
-              height="300"
-              allowFullScreen=""
-              loading="lazy"
-              reFerrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </a>
-          <button onClick={handleAddToCalendar}>Agregar a calendario</button>
+          </div>
+          <div className="celebration">
+            <p className='celebration__title'>Celebración</p>
+            <a href="https://maps.app.goo.gl/UNmd2EcyWEU4KLT68" target="_blank">
+              <img src={leoch} alt="" className="celebration__img" /></a>
+              <p className='celebration__castle'>Castillo de Doune</p>
+              <p className='celebration__time'>20:00</p>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2220.3748460488637!2d-4.0525207230910185!3d56.185198873247344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48888bd24de4fcbf%3A0x5599d30a61b7fb39!2sCastillo%20de%20Doune!5e0!3m2!1ses!2ses!4v1698691886282!5m2!1ses!2ses"
+                width="400"
+                height="300"
+                allowFullScreen=""
+                loading="lazy"
+                reFerrerPolicy="no-referrer-when-downgrade"
+                className='celebration__map'
+              ></iframe>
+            </div>
+            <button className="section-places__btn" onClick={handleAddToCalendar}>Agregar a calendario</button>
+          
         </section>
-        <section>
-          <h2>Compartiendo este día junto a tí</h2>
-          <p>
+        <section className='photos'>
+          <h2 className='photos__title'>Compartiendo este día junto a tí</h2>
+          <p className='photos__subtitle'>
             Como seguro que todos hacéis muchas fotos y no queremos
             perdérnoslas. Hemos creado un album compartido en Wedshoots
           </p>
-          <ol>
-            <li>
-              Descarga la App de Wedshoots: <a href="#">www.wedshoots.com</a>
+          <ul className='photos__wedshoots'>
+            <li className='photos__wedshoots--p'>
+              Descarga la App de <a href="www.wedshoots.com">Wedshoots</a>
             </li>
-            <li>
-              Accede como invitado al albun de la boda con el código:{' '}
-              <a href="#">AB158Y</a>{' '}
+            <li className='photos__wedshoots--p'>
+              Accede como invitado al albun de la boda con el código:
+              <a href="#">AB158Y</a>
             </li>
-            <li>
+            <li className='photos__wedshoots--p'>
               Sube todas las fotos que quieras, antes, durante y después de la
               boda.
             </li>
-          </ol>
-          <p>
+          </ul>
+          <p className='photos__ig'><i className="fa-brands fa-instagram photos__ig--icon"></i>
             ¿No quieres descargarte wedshoots?, podrás utilizar el #jamie&claire
           </p>
         </section>
